174a121c600a847c89d6d756846482b471614c92
= <jonathan.r.paton@googlemail.com>
2019-04-10 16:20:50 +0100

    Add TMXLITE_ROOT variable for finding in custom locations

--- cmake/modules/FindTMXLITE.cmake
+++ cmake/modules/FindTMXLITE.cmake
@@ -1,10 +1,14 @@
 include(FindPackageHandleStandardArgs)
 
 # Search for the header file
-find_path(TMXLITE_INCLUDE_DIR NAMES tmxlite/Config.hpp PATH_SUFFIXES include)
+find_path(TMXLITE_INCLUDE_DIR NAMES tmxlite/Config.hpp 
+                              PATH_SUFFIXES include
+                              HINTS ${TMXLITE_ROOT})
 
 # Search for the library
-find_library(TMXLITE_LIBRARIES NAMES tmxlite PATH_SUFFIXES lib)
+find_library(TMXLITE_LIBRARIES NAMES tmxlite 
+                                     PATH_SUFFIXES lib
+                                     HINTS ${TMXLITE_ROOT})
 
 # Did we find everything we need?
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(tmxlite DEFAULT_MSG TMXLITE_LIBRARIES TMXLITE_INCLUDE_DIR) 
